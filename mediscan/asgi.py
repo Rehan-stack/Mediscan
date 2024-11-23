28a22c60b5e1170d54949c8b6399fdc5971baa07
Rehan Ahmed <73068765+Rehan-stack@users.noreply.github.com>
2024-11-23 16:30:36 +0500

    first commit

---
+++ mediscan/asgi.py
@@ -0,0 +1,32 @@
+"""
+ASGI config for healthstack project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mediscan.settings')
+
+# application = get_asgi_application()
+import os
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+import doctor.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mediscan.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            doctor.routing.websocket_urlpatterns
+        )
+    ),
+})
